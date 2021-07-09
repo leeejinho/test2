7c6703aefe146375b7dbe163dafe8e5dd880dd80
jihyun00000 <wlgusgustjd@naver.com>
2021-07-09 21:13:21 +0900

    MonsterBullet

--- Shooting_Game/Monster.cpp
+++ Shooting_Game/Monster.cpp
@@ -5,7 +5,7 @@
 #include "DeadEffect.h"
 
 CMonster::CMonster()
-	: m_bDiagonal(true), m_bRotation(true), m_fParentX(0.f), m_fParentY(0.f), m_eCurState(END)
+	: m_bDiagonal(true), m_bRotation(true), m_fParentX(0.f), m_fParentY(0.f), m_eCurState(END), m_dwDescent(0), m_bStop(false), m_bDescentRot(true)
 {
 }
 
@@ -19,7 +19,7 @@ HRESULT CMonster::Initialize()
 {
 	m_tInfo.vPos = { 400.f, 0.f, 0.f };
 	m_tInfo.vDir = D3DXVECTOR3(1.f, 1.f, 0.f);
-	m_tInfo.vSize = D3DXVECTOR3(40.f, 40.f, 0.f);
+	m_tInfo.vSize = D3DXVECTOR3(30.f, 40.f, 0.f);
 
 	m_vP[0] = { -m_tInfo.vSize.x * 0.5f, -m_tInfo.vSize.y * 0.5f, 0.f };
 	m_vP[1] = { m_tInfo.vSize.x * 0.5f, -m_tInfo.vSize.y * 0.5f, 0.f };
@@ -37,20 +37,18 @@ int CMonster::Update()
 	int i = 0;
 	if (m_bDead)
 	{
-		while (i < 100)
-		{
-			CObjMgr::Get_Instance()->Add_Object(CAbstractFactory<CDeadEffect>::Create(this), OBJID::EFFECT);
-			++i;
-		}
-
+		Play_Dead_Effect(this);
 		return OBJ_DEAD;
 	}
-
-	if(m_eCurState == LEFT)
-		Create_Monster_Left();
-	if(m_eCurState == RIGHT)
-		Create_Monster_Right();
-
+	if (!m_bStop)
+	{
+		if (m_eCurState == LEFT)
+			Create_Monster_Left();
+		if (m_eCurState == RIGHT)
+			Create_Monster_Right();
+	}
+	else
+		Monster_Descent();
 	return OBJ_NOEVENT;
 }
 
@@ -128,6 +126,14 @@ int CMonster::Create_Monster_Left()
 
 				if (fDist > 1.f)
 					m_tInfo.vPos += m_tInfo.vDir * m_fSpeed;
+				else
+				{
+					m_tInfo.vPos = m_pTargetPos;
+					m_bStop = true;
+					m_fAngle = 0.f;
+					m_vP[4] = { 0.f, 0.f, 0.f };
+					m_dwDescent = GetTickCount();
+				}
 			}
 		}
 	}
@@ -194,6 +200,14 @@ int CMonster::Create_Monster_Right()
 
 				if (fDist > 1.f)
 					m_tInfo.vPos += m_tInfo.vDir * m_fSpeed;
+				else
+				{
+					m_tInfo.vPos = m_pTargetPos;
+					m_bStop = true;
+					m_fAngle = 0.f;
+					m_vP[4] = { 0.f, 0.f, 0.f };
+					m_dwDescent = GetTickCount();
+				}
 			}
 		}
 	}
@@ -204,3 +218,62 @@ int CMonster::Create_Monster_Right()
 	return 0;
 }
 
+void CMonster::Monster_Descent()
+{
+	D3DXMATRIX matParentTrans;
+	D3DXMATRIX matTrans, matRelRotZ, matWorld;
+	int Delay = 5000;
+	int iCount = 0;
+
+	if (m_dwDescent + Delay < GetTickCount())
+	{
+		if (m_bDescentRot)
+		{
+			m_fAngle += 2.f;
+			if (m_tInfo.vPos.x < (WINCX >> 1))
+			{
+				D3DXMatrixTranslation(&matTrans, 50.f, 0.f, 0.f);
+				D3DXMatrixTranslation(&matParentTrans, m_tInfo.vPos.x - 50.f, m_tInfo.vPos.y, 0.f);
+				D3DXMatrixRotationZ(&matRelRotZ, D3DXToRadian(-m_fAngle));
+			}
+			else
+			{
+				D3DXMatrixTranslation(&matTrans, -50.f, 0.f, 0.f);
+				D3DXMatrixTranslation(&matParentTrans, m_tInfo.vPos.x + 50.f, m_tInfo.vPos.y, 0.f);
+				D3DXMatrixRotationZ(&matRelRotZ, D3DXToRadian(m_fAngle));
+			}
+			matWorld = matTrans * matRelRotZ * matParentTrans;
+
+			if (m_fAngle > 180)
+			{
+				m_tInfo.vPos = m_vQ[4];
+				m_tInfo.vDir = CObjMgr::Get_Instance()->Get_Player()->Get_Info().vPos - m_vQ[4];
+				D3DXVec3Normalize(&m_tInfo.vDir, &m_tInfo.vDir);
+				while (iCount < 3)
+				{
+					CObj* pObj = CAbstractFactory<CMonsterBullet>::Create(m_tInfo.vPos.x, m_tInfo.vPos.y);
+					CObjMgr::Get_Instance()->Add_Object(pObj, OBJID::MONSTERBULLIT);
+					++iCount;
+				}
+				m_bDescentRot = !m_bDescentRot;
+			}
+		}
+		else
+		{
+			m_tInfo.vPos += m_tInfo.vDir * m_fSpeed;
+			D3DXMatrixTranslation(&matTrans, m_tInfo.vPos.x, m_tInfo.vPos.y, 0.f);
+			matWorld = matTrans;
+		}
+	}
+	else
+	{
+		D3DXMatrixTranslation(&matTrans, m_tInfo.vPos.x, m_tInfo.vPos.y, 0.f);
+		matWorld = matTrans;
+	}
+
+	for (int i = 0; i < 5; ++i)
+		D3DXVec3TransformCoord(&m_vQ[i], &m_vP[i], &matWorld);
+
+	if (m_tInfo.vPos.y > WINCY + 200)
+		m_bDead = true;
+}
--- Shooting_Game/Monster.h
+++ Shooting_Game/Monster.h
@@ -27,12 +27,18 @@ public:
 	int Create_Monster_Left();
 	int Create_Monster_Right();
 
+	void Monster_Descent();
+
 
 private:
+	DWORD		m_dwDescent;
+	bool		m_bDescentRot;
+
 	bool		m_bDiagonal;		// 대각선 
 	bool		m_bRotation;		// 회전
 	float		m_fParentX;			// 부모 x축
 	float		m_fParentY;			// 부모 y축
+	bool		m_bStop;
 	D3DXVECTOR3	m_pTargetPos;
 
 	STATE		m_eCurState;
